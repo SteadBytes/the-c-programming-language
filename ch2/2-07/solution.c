41e719bb9f8bcaa2c6509836edfa52159b69b272
Ben Steadman <steadmanben1@gmail.com>
2019-05-23 19:37:43 +0100

    2-07

---
+++ ch2/2-07/solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+int main(void)
+{
+    printf("invert(32, 3, 2) -> %u\n", invert(32, 3, 2));
+    return 0;
+}
+
+
+/* invert: return x with the n bits beginning at position p inverted */
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned n_mask =  ~(~0 << n);
+    int shift = p + 1 - n;
+    return x ^ (n_mask << shift);
+}
